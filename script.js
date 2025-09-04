ad2fd3870174e238ff76bdd28fdffd2144c3eaf8
Jesus Moreno <Jesusytty@gmail.com>
2025-09-04 16:44:54 -0500

    Cambio total

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("infoBtn");
+
+  button.addEventListener("click", () => {
+    alert("El Estadio Metropolitano Roberto Meléndez es el estadio más grande de Colombia, ubicado en Barranquilla. ¡Es la casa de la Selección Colombia!");
+  });
+});
